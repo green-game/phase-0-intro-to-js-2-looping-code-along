e2dac3aee0f0661f06467a030e9b768e65f3a1cf
Allan Monzon <greengamer.am@gmail.com>
2022-11-25 18:03:25 -0500

    commit tree f616e5a79b6d4d20e353e09296489cd2c3a1d2c0

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solutions in this file
+function writeCards( array,event){
+    let newArray = []
+       for (let P = 0;P < array.length;P++){
+           
+    newArray.push(`Thank you, ${array[P]}, for the wonderful ${event} gift!`)
+    
+   };
+   
+   return newArray};
+   
+   
+   function countDown(count){
+    
+       while (count > -1) {
+         console.log(count--);
+       }
+   }
+   
